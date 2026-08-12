d626f4e7459fcbf7b760e5f189e9441e70028bdc
dennisvink <11063042+dennisvink@users.noreply.github.com>
2026-08-12 12:20:30 +0000

    Update hurozoctl to 0.1.55

--- Formula/hurozoctl.rb
+++ Formula/hurozoctl.rb
@@ -1,15 +1,15 @@
 class Hurozoctl < Formula
   desc "Administrative CLI for Hurozo Deployment Centers"
   homepage "https://github.com/hurozo/hurozoctl"
-  version "0.1.0"
+  version "0.1.55"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/hurozo/hurozoctl/releases/download/v0.1.0/hurozoctl_0.1.0_darwin_arm64.tar.gz"
-      sha256 "ARM64_SHA256_PENDING_FIRST_RELEASE"
+      url "https://github.com/hurozo/hurozoctl/releases/download/v0.1.55/hurozoctl_0.1.55_darwin_arm64.tar.gz"
+      sha256 "095cac8166821d20a8bd6e79db75f7a8b359eec3352d9022df6f8e4fb27873bf"
     else
-      url "https://github.com/hurozo/hurozoctl/releases/download/v0.1.0/hurozoctl_0.1.0_darwin_amd64.tar.gz"
-      sha256 "AMD64_SHA256_PENDING_FIRST_RELEASE"
+      url "https://github.com/hurozo/hurozoctl/releases/download/v0.1.55/hurozoctl_0.1.55_darwin_amd64.tar.gz"
+      sha256 "a09e80e17976cf7192cc5780e97cfb95a43251e77f346a5c6ac81969766061b3"
     end
   end
 
